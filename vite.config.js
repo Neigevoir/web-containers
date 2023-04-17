4926686a6fa1cbd9722fd0d964c0ce175b27f750
radis <luobobo@radiss-iMac.local>
2023-04-17 16:01:34 +0800

    fix: build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        loading: resolve(__dirname, "loading.html"),
+      },
+    },
+  },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
